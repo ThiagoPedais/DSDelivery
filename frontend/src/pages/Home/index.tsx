233f385c80c0094928f0991bc6dc1978d1523aa5
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-08-04 21:32:32 -0300

    Responsive

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,26 +6,29 @@ export default function Home() {
     return (
         <div className="container home-container">
             <div className="home-content">
-                <div className="home-actions">
-                    <div className="d-flex">
+                {/* <div className="home-actions"> */}
+                    <div className="h d-flex">
                         <div className="titles">
+                            <div className="only-titles">
                             <h1 className="home-title">
-                                Faça seu pedido <br /> que entregamos <br /> pra você !!
+                                Faça seu pedido que entregamos pra você !!
                             </h1>
                             <h3 className="home-subtitle">
-                                Escolha o seu pedido e em poucos minutos <br /> levaremos na sua porta
+                                Escolha o seu pedido e em poucos minutos levaremos na sua porta
                             </h3>
+                            </div>
+                          
                             <Link to="/orders">
                                 <button className="home-btn-order">Fazer pedido</button>
                             </Link>
                         </div>
                         <div className="home-image">
-                            <img src={main} alt="img-main" />
+                            <img src={main} alt="img-main" className="igmSvg" />
                         </div>
                     </div>
 
 
-                </div>
+                {/* </div> */}
             </div>
         </div>
     )
